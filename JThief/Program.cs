0f2451612cbc7784a38f5d07f5e33d0ed2c39cbc
chajus1 <chajus1@googlemail.com>
2023-06-22 00:54:19 +0100

    Add project files.

---
+++ JThief/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JThief
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
